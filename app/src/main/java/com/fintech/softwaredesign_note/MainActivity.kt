857265c65cf5480172353f18e8f7ed82e3e6ac07
daniilxt <daniil-firsov@mail.ru>
2020-09-28 20:59:37 +0300

    initial commit

---
+++ app/src/main/java/com/fintech/softwaredesign_note/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fintech.softwaredesign_note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
