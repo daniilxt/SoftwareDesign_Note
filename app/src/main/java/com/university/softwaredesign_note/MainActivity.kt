6628c7d4257a456a8684717dcfa5fff41e22b398
daniilxt <daniil-firsov@mail.ru>
2020-10-05 14:49:18 +0300

    test fab + curved nav view

--- app/src/main/java/com/university/softwaredesign_note/MainActivity.kt
+++ app/src/main/java/com/university/softwaredesign_note/MainActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import meow.bottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.customBottomBar)
         val navController = findNavController(R.id.main_activity__container)
-        bottomNavigationView.setupWithNavController(navController)*/
+        bottomNavigationView.setupWithNavController(navController)
+
+//        bottomNavigationView.add(MeowBottomNavigation.Model(1, R.drawable.music))
+      /*  bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.bottom_nav__like))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.bottom_nav__layers))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.bottom_nav__search))*/
     }
 }
